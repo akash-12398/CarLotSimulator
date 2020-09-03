2f785963e70e5a6cb9469b6f5624d9d7b85552fe
akash-12398 <napinderbajwa@gmail.com>
2020-09-02 22:51:20 -0500

    https://github.com/akash-12398/CarLotSimulator.git.

---
+++ CarLotSimulator/Carlot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+        public CarLot()
+        {
+        }
+        
+
+        public List<Car> Cars = new List<Car>();
+
+        public static int numberOfCars = 1;
+
+
+    }
+}
--- CarLotSimulator/Program.cs
+++ CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -18,6 +19,55 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            var lot = new CarLot();
+
+            //var list = new List<Car>();
+           
+
+
+            var camry = new Car();
+
+            camry.Make = "Toyota";
+            camry.Model = "Camry";
+            camry.EngineNoise = "vroom";
+            camry.Year = 2015;
+            camry.HonkNoise = "beep";
+            camry.IsDriveable = true;
+
+            lot.Cars.Add(camry);
+
+
+
+            var rinkucar = new Car()
+            {
+                Make = "Tesla",
+                Model = "Cyber Truck",
+                EngineNoise = "Ding",
+                Year = 2019,
+                HonkNoise = "song",
+                IsDriveable = false
+            };
+
+
+            lot.Cars.Add(rinkucar);
+
+
+            var akashCar = new Car (2013, "Nissan","Rogue","vroom","varuga", false);
+
+
+
+            lot.Cars.Add(akashCar);
+
+
+
+            foreach (var car in lot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year} Make:{car.Make} Model:{car.Model}");
+
+            }
+            
+
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
@@ -29,5 +79,7 @@ namespace CarLotSimulator
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
         }
+
+       
     }
 }
